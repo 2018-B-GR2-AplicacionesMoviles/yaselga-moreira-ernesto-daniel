879a4ff44d431439a0f45944ee721f6c0aaab74f
Ernesto Yaselga <eyaselga@hotmail.com>
2018-11-16 11:11:39 -0500

    deber inicio

---
+++ 02-android/deber161118/app/src/main/java/ernesteins/deber161118/MainActivity.kt
@@ -0,0 +1,12 @@
+package ernesteins.deber161118
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
